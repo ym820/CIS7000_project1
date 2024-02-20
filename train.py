fc386fbe11b63712a561205c0c18105407b3b7f1
Yikai Mao <yikai.mao@outlook.com>
2024-02-20 12:44:55 -0500

    Update wandb info

--- train.py
+++ train.py
@@ -32,10 +32,10 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_entity = 'ssdd'
+wandb_entity = 'cis7000_project1'
 wandb_log = False # disabled by default
-wandb_project = 'owt'
-wandb_run_name = 'gpt2' # 'run' + str(time.time())
+wandb_project = 'teaching_arithmetic'
+wandb_run_name = 'run' + str(time.time())
 exp_name = 'default_exp_name'
 # data
 dataset = 'bal'
@@ -94,7 +94,8 @@ min_lr = None # minimum learning rate, should be ~= learning_rate/10 per Chinchi
 backend = 'nccl' # 'nccl', 'gloo', etc.
 # system
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
-dtype = 'bfloat16' if torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+# dtype = 'bfloat16' if torch.cuda.is_bf16_supported() else 'float16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
+dtype = 'float16'
 compile = True # use PyTorch 2.0 to compile the model to be faster
 use_flash = True
 data_type = 'binary' # 'binary' by default, can be 'text'
